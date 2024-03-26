21ce81c34c0b684e0af0b6d57307467b66aa30e6
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-26 15:53:14 +0100

    Window show application instead of path

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -1,8 +1,8 @@
 show_window_current_format() {
   local number="#I"
-  local color=$(get_tmux_option "@dracuccin_window_current_color" "$thm_orange")
+  local color=$(get_tmux_option "@dracuccin_window_current_color" "$thm_pink")
   local background=$(get_tmux_option "@dracuccin_window_current_background" "$thm_bg")
-  local text="$(get_tmux_option "@dracuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  local text="$(get_tmux_option "@dracuccin_window_current_text" "#W")" # use #W for application instead of directory
   local fill="$(get_tmux_option "@dracuccin_window_current_fill" "number")" # number, all, none
 
   local current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
