23391147cf8c47569d308b99749c6fb1c71eda7c
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-26 15:52:59 +0100

    Change session colours

--- status/session.sh
+++ status/session.sh
@@ -1,7 +1,7 @@
 show_session() {
   local index=$1
   local icon=$(get_tmux_option "@dracuccin_session_icon" "")
-  local color=$(get_tmux_option "@dracuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
+  local color=$(get_tmux_option "@dracuccin_session_color" "#{?client_prefix,$thm_green,$thm_blue}")
   local text=$(get_tmux_option "@dracuccin_session_text" "#S")
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
